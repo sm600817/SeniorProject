4d02c043acbf450f626a555daf2e9f3634892e2a
Sean_Monahan <sm600817@sju.edu>
2017-01-28 12:16:42 -0500

    Added sql file to dump team data into teams table

---
+++ nfl_picks_db/teams_data.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.2
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Jan 28, 2017 at 06:12 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Dumping data for table `teams`
+--
+
+INSERT INTO `teams` (`team_num`, `team_city`, `team_name`, `wins`, `losses`, `ties`, `offPct`) VALUES
+(1, 'Philadelphia', 'Eagles', 0, 0, 0, 19),
+(2, 'New York', 'Giants', 0, 0, 0, 17),
+(3, 'Dallas', 'Cowboys', 0, 0, 0, 29),
+(4, 'Washington', 'Redskins', 0, 0, 0, 26),
+(5, 'Seattle', 'Seahawks', 0, 0, 0, 20),
+(6, 'Arizona', 'Cardinals', 0, 0, 0, 25),
+(7, 'Los Angeles', 'Rams', 0, 0, 0, 12),
+(8, 'San Francisco', '49ers', 0, 0, 0, 18),
+(9, 'Green Bay', 'Packers', 0, 0, 0, 31),
+(10, 'Detroit', 'Lions', 0, 0, 0, 22),
+(11, 'Minnesota', 'Vikings', 0, 0, 0, 18),
+(12, 'Chicago', 'Bears', 0, 0, 0, 17),
+(13, 'Atlanta', 'Falcons', 0, 0, 0, 35),
+(14, 'Tampa Bay', 'Buccanneers', 0, 0, 0, 21),
+(15, 'New Orleans', 'Saints', 0, 0, 0, 31),
+(16, 'Carolina', 'Panthers', 0, 0, 0, 20),
+(17, 'New England', 'Patriots', 0, 0, 0, 30),
+(18, 'Miami', 'Dolphins', 0, 0, 0, 22),
+(19, 'Buffalo', 'Bills', 0, 0, 0, 26),
+(20, 'New York', 'Jets', 0, 0, 0, 15),
+(21, 'Kansas City', 'Chiefs', 0, 0, 0, 20),
+(22, 'Oakland', 'Raiders', 0, 0, 0, 24),
+(23, 'Denver', 'Broncos', 0, 0, 0, 16),
+(24, 'San Diego', 'Chargers', 0, 0, 0, 24),
+(25, 'Pittsburg', 'Steelers', 0, 0, 0, 26),
+(26, 'Baltimore', 'Ravens', 0, 0, 0, 16),
+(27, 'Cincinnati', 'Bengals', 0, 0, 0, 20),
+(28, 'Cleveland', 'Browns', 0, 0, 0, 15),
+(29, 'Houston', 'Texans', 0, 0, 0, 17),
+(30, 'Tennesse', 'Titans', 0, 0, 0, 25),
+(31, 'Indianapolis', 'Colts', 0, 0, 0, 26),
+(32, 'Jacksonville', 'Jaguars', 0, 0, 0, 17);
