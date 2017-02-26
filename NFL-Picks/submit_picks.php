2b3562beb0822605e076caffb92955298185f9d5
Sean_Monahan <sm600817@sju.edu>
2017-02-26 14:45:31 -0500

    Changed message when user did not make a pick

--- NFL-Picks/submit_picks.php
+++ NFL-Picks/submit_picks.php
@@ -9,44 +9,52 @@
 	$pool = $_POST['pool'];
 	$pick = $_POST['pick'];
 
-	$sql = "SELECT user
-			FROM picks
-			WHERE user ='$user'
-			AND week = $week
-			AND pool_id = $pool";
-	$result = mysqli_query($conn, $sql);
+	if($pick == 'undefined'){
+		echo "<div class='alert alert-warning alert-dismissible'>
+                <strong>Whoops!</strong> You did not make a pick
+            </div>";
+	}
+	else{
 
-	if(mysqli_num_rows($result) > 0){
-		$sql = "UPDATE picks
-				SET team = $pick, game = $game
+		$sql = "SELECT user
+				FROM picks
 				WHERE user ='$user'
 				AND week = $week
 				AND pool_id = $pool";
-		mysqli_query($conn, $sql);
-		if(mysqli_affected_rows($conn) > 0){
-			echo "<div class='span4 offset4 alert alert-success alert-dismissible'>
-                <strong>Saved!</strong> Your pick was saved successfully
-            </div>";
+		$result = mysqli_query($conn, $sql);
+
+		if(mysqli_num_rows($result) > 0){
+			$sql = "UPDATE picks
+					SET team = $pick, game = $game
+					WHERE user ='$user'
+					AND week = $week
+					AND pool_id = $pool";
+			mysqli_query($conn, $sql);
+			if(mysqli_affected_rows($conn) > 0){
+				echo "<div class='span4 offset4 alert alert-success alert-dismissible'>
+	                <strong>Saved!</strong> Your pick was saved successfully
+	            </div>";
+			}
+			else{
+				echo "<div class='alert alert-warning alert-dismissible'>
+	                <strong>Sorry!</strong> There was a problem saving your pick
+	            </div>";
+			}
 		}
 		else{
-			echo "<div class='alert alert-warning alert-dismissible'>
-                <strong>Sorry!</strong> There was a problem saving your pick
-            </div>";
-		}
-	}
-	else{
-		$sql = "INSERT INTO picks (user, week, game, pool_id, team)
-				VALUES ('$user', $week, $game, $pool, $pick)";
-		mysqli_query($conn, $sql);
-		if(mysqli_affected_rows($conn) > 0){
-			echo "<div class='span4 offset4 alert alert-success alert-dismissible'>
-                <strong>Saved!</strong> Your pick was saved successfully
-            </div>";
-		}
-		else{
-			echo "<div class='alert alert-warning alert-dismissible'>
-                <strong>Sorry!</strong> There was a problem saving your pick
-            </div>";
+			$sql = "INSERT INTO picks (user, week, game, pool_id, team)
+					VALUES ('$user', $week, $game, $pool, $pick)";
+			mysqli_query($conn, $sql);
+			if(mysqli_affected_rows($conn) > 0){
+				echo "<div class='span4 offset4 alert alert-success alert-dismissible'>
+	                <strong>Saved!</strong> Your pick was saved successfully
+	            </div>";
+			}
+			else{
+				echo "<div class='alert alert-warning alert-dismissible'>
+	                <strong>Sorry!</strong> There was a problem saving your pick
+	            </div>";
+			}
 		}
 	}
 ?>
