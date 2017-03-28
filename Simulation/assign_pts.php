531566190f703e50de3cfc2b313906ea6c1a931c
Sean_Monahan <sm600817@sju.edu>
2017-03-28 15:44:03 -0400

    Small code structure fix in assign_pts.php

--- Simulation/assign_pts.php
+++ Simulation/assign_pts.php
@@ -1,15 +1,11 @@
 <?php
 
 include __DIR__ . '/../DBConnect.php';
-
 $week = $_GET['week'];
-
 $sql = "SELECT user, pool_id, week, game, team, pts_assigned
 		FROM picks
 		WHERE week = $week";
-
 $result = mysqli_query($conn, $sql);
-
 if (mysqli_num_rows($result) > 0) {
 	while($row = mysqli_fetch_assoc($result)){
 		$user = $row['user'];
@@ -17,25 +13,20 @@ if (mysqli_num_rows($result) > 0) {
 		$game = $row['game'];
 		$team = $row['team'];
 		$pts_assigned = $row['pts_assigned'];
-
 		if($pts_assigned == 0){
 			$sql = "SELECT game_id, home_team, away_team, home_score, away_score
 				FROM games
 				WHERE game_id = $game";
-
 			$game_result = mysqli_query($conn, $sql);
 			$game_row = mysqli_fetch_assoc($game_result);
-
 			$homeId = $game_row['home_team'];
 			$homeScore = $game_row['home_score'];
-
 			$awayId = $game_row['away_team'];
 			$awayScore = $game_row['away_score'];
-
 			if($team == $homeId){
 				if($homeScore > $awayScore){
 					$sql = "UPDATE scores
-							SET total_score = total_score + $homeScore, correct_picks = correct_picks + 1
+							SET total_score = total_score + $homeScore
 							WHERE pool_id = $pool
 							AND user = '$user'";
 					mysqli_query($conn, $sql);
@@ -44,13 +35,12 @@ if (mysqli_num_rows($result) > 0) {
 			if($team == $awayScore){
 				if($awayScore > $homeScore){
 					$sql = "UPDATE scores
-							SET total_score = total_score + $awayScore, correct_picks = correct_picks + 1
+							SET total_score = total_score + $awayScore
 							WHERE pool_id = $pool
 							AND user = '$user'";
 					mysqli_query($conn, $sql);
 				}
 			}
-
 			$sql = "UPDATE picks
 					SET pts_assigned = 1
 					WHERE user = '$user' 
@@ -58,8 +48,12 @@ if (mysqli_num_rows($result) > 0) {
 					AND week = $week";
 			mysqli_query($conn, $sql);
 		}
-
 	}
+	$sql = "UPDATE weeks
+			SET was_played = 1
+			WHERE week_id = $week";
+	mysqli_query($conn, $sql);
+
 	if($week == 17){
 		$sql = "SELECT pool_id, total_pot
 				FROM pools";
@@ -343,6 +337,9 @@ if (mysqli_num_rows($result) > 0) {
 		}		
 
 	}
+	
+	echo "All points have been assigned";
+	
 }
 else{
 	echo "There was a problem assigning points";
